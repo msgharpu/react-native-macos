88087c23aee79fdd8d12198bb51ad4cbb9cc81e8
Saad Najmi <sanajmi@microsoft.com>
2021-06-11 13:31:31 -0500

    Revert "Post a notification when the global JS runtime vars finish initializing  (#761)"
    This reverts commit 202545e020be9e2053af80a0665a859ca3ed144a.

--- ReactCommon/jsiexecutor/jsireact/JSIExecutor.cpp
+++ ReactCommon/jsiexecutor/jsireact/JSIExecutor.cpp
@@ -6,7 +6,6 @@
  */
 
 #include "jsireact/JSIExecutor.h"
-#include "JSIRuntimeInitializeStateNotifier-C-Interface.h"
 
 #include <cxxreact/JSBigString.h>
 #include <cxxreact/ModuleRegistry.h>
@@ -142,10 +141,6 @@ void JSIExecutor::loadApplicationScript(
   if (runtimeInstaller_) {
     runtimeInstaller_(*runtime_);
   }
-
-  // TODO(OSS Candidate ISS#2710739)- send a notification that the global variables are set so we can then proceed with bundle loading (which depends on these and will crash if that thread finishes before they are set)
-  NotifyRuntimeInitializationEnd();
-
   bool hasLogger(ReactMarker::logTaggedMarker);
   std::string scriptName = simpleBasename(sourceURL);
   if (hasLogger) {
